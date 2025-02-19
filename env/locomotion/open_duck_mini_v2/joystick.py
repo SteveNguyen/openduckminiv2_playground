0cc72b2c15dfe1fda1388d4dfad9dc492d99e488
apirrone <awanegenebayo@gmail.com>
2025-02-19 13:39:39 +0100

    more delay

--- env/locomotion/open_duck_mini_v2/joystick.py
+++ env/locomotion/open_duck_mini_v2/joystick.py
@@ -54,9 +54,9 @@ def default_config() -> config_dict.ConfigDict:
       noise_config=config_dict.create(
           level=1.0,  # Set to 0.0 to disable noise.
           action_min_delay=0,  # env steps
-          action_max_delay=2,  # env steps
+          action_max_delay=4,  # env steps
           imu_min_delay=0,  # env steps
-          imu_max_delay=2,  # env steps
+          imu_max_delay=4,  # env steps
           scales=config_dict.create(
               hip_pos=0.03,  # rad #for each hip joint
               kfe_pos=0.05,  # kfe=Knee Pitch
