b0bce9f7c8e83ecada2ff40c3d0893d671a1f4b5
apirrone <awanegenebayo@gmail.com>
2025-02-23 14:47:02 +0100

    action scale 0.5, more rand

--- env/locomotion/open_duck_mini_v2/joystick.py
+++ env/locomotion/open_duck_mini_v2/joystick.py
@@ -42,7 +42,7 @@ def default_config() -> config_dict.ConfigDict:
       sim_dt=0.002,
       episode_length=450,
       action_repeat=1,
-      action_scale=0.25,
+      action_scale=0.5,
       dof_vel_scale=0.05,
       history_len=0,
       soft_joint_pos_limit_factor=0.95,
@@ -76,7 +76,7 @@ def default_config() -> config_dict.ConfigDict:
               base_y_swing=0.0,
               # Energy related rewards.
               torques=-1.0e-3,
-              action_rate=-0.375,  # was -1.5
+              action_rate=-0.75,  # was -1.5
               energy=0.0,
               # Feet related rewards.
               feet_clearance=0.0,
